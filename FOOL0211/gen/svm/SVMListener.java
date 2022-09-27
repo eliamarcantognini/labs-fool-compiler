4a2bd65f524bbf83dcbe35cb7e7f7d1b78383d76
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-09-27 14:31:12 +0200

    add sources to 0211

---
+++ FOOL0211/gen/svm/SVMListener.java
@@ -0,0 +1,33 @@
+// Generated from G:/OneDrive - Alma Mater Studiorum Università di Bologna/LCMC/Lab/Workspace/labs-fool-compiler/FOOL0211/src/svm\SVM.g4 by ANTLR 4.10.1
+package svm;
+
+import java.util.HashMap;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link SVMParser}.
+ */
+public interface SVMListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void enterInstruction(SVMParser.InstructionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void exitInstruction(SVMParser.InstructionContext ctx);
+}
