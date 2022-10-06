b848454ef05cd631c14d486f3e39d00a65115c99
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-10-06 15:45:14 +0200

    0612 - init project

---
+++ FOOL0612/gen/svm/SVMVisitor.java
@@ -0,0 +1,28 @@
+// Generated from G:/OneDrive - Alma Mater Studiorum Università di Bologna/LCMC/Lab/Workspace/labs-fool-compiler/FOOL0612/src/svm\SVM.g4 by ANTLR 4.10.1
+package svm;
+
+import java.util.*;
+
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link SVMParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface SVMVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInstruction(SVMParser.InstructionContext ctx);
+}
